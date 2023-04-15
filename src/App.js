0e17c53aa7475414f51c50b143e874937e74b752
DinhPhi9x <trumrau95@example.com>
2023-04-15 10:42:21 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
 // rfc
 export default function App() {
-const [sum, setSum] = useState(0);
+const [sum, setSum] = useState(null);
 const [SoA, setSoA] = useState(0);
 const [SoB, setSoB] = useState(0);
+const handleSum = () => {
+    setSum (Number(SoA) +  Number(SoB));
+}
   return (
       <>
           <div className="container">
               <div className="form-group">
-                  Số A: <input type="number" className="form-control" />
+                  Số A: <input type="number" className="form-control" 
+            onChange={(e)=>setSoA(e.target.value)}/>
               </div>
 
               <div className="form-group">
-                  Số B: <input type="number" className="form-control" />
+                  Số B: <input type="number" className="form-control"
+                  onChange={(e)=>setSoB(e.target.value)} />
               </div>
               <div className="form-group">
-
+               Tong : {sum}
               </div>
               <div className="form-group">
-                  <button className="btn btn-primary">Tính</button>
+                  <button className="btn btn-primary" onClick={handleSum}>Tính</button>
               </div>
           </div>
       </>
